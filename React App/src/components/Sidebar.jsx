971c075b512c49507e8bf247288aa2f1b2a6ff10
IZ10na <152287176+IZ10na@users.noreply.github.com>
2024-09-13 09:52:52 +0200

    Add files via upload

--- React App/src/components/Sidebar.jsx
+++ React App/src/components/Sidebar.jsx
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
 import {
-    FaTh,
+    FaTicketAlt,
+    // FaTh,
     FaBars,
     FaUserAlt,
     FaRegChartBar,
     // FaCommentAlt,
     FaShoppingBag,
-    FaThList,
     FaFilm,
     FaBullhorn,
+    FaSignInAlt,
     FaMapMarkerAlt,
     // FaSignInAlt,
     // FaUserPlus 
 }from "react-icons/fa";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 
 
 const Sidebar = ({children}) => {
     const[isOpen ,setIsOpen] = useState(false);
     const toggle = () => setIsOpen (!isOpen);
     const menuItem=[
+        // {
+        //     path:"/",
+        //     name:"Dashboard",
+        //     icon:<FaTh/>
+        // },
         {
-            path:"/Location",
+            path:"/location",
             name:"Location",
             icon:<FaMapMarkerAlt/>
         },
@@ -54,21 +60,31 @@ const Sidebar = ({children}) => {
         {
             path:"/ticketsList",
             name:"Tickets List",
-            icon:<FaThList/>
+            icon:<FaTicketAlt/>
+        },
+        {
+            path:"/login",
+            name:"Log In",
+            icon:<FaSignInAlt />
         },
     ]
+    // const user = JSON.parse(localStorage.getItem('user'));
+
     return (
+        
         <div className="container">
            <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
                <div className="top_section">
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                   {/* <h1 style={{display: isOpen ? "block" : "none"}} className="logo"><img src="logo.jpg" alt="" /></h1> */}
+        
+                  {/* <h1>Cinema</h1>  */}
                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
                        <FaBars onClick={toggle}/>
                    </div>
                </div>
                {
                    menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                       <NavLink to={item.path} key={index} className="link" activeClassName="active">
                            <div className="icon">{item.icon}</div>
                            <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
                        </NavLink>
